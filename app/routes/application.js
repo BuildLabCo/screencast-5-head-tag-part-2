b61f3cfe0bfb8efbf039a4b8ca79f2554845c674
Kyle Davis <validkeys@gmail.com>
2015-06-18 16:04:23 -0400

    Removed content security policy. Refactored meta to be a prop and use the didTransition action to render them

--- app/routes/application.js
+++ app/routes/application.js
@@ -8,4 +8,23 @@ export default Ember.Route.extend({
 
     return (hasTokens) ? base + " / " + tokens.join(" / ") : base;
   },
+
+  actions: {
+    didTransition: function() {
+      // We'll assume that only the current route's meta should be applied
+      let handlers    = this.router.get('router.currentHandlerInfos'),
+          currentLeaf = handlers[handlers.length - 1],
+          elements    = [];
+
+      // So at this point our meta is being added
+      // but when we click in to the person route, we can see that
+      // our meta is not being cleaned up. So let's clean up
+      if (currentLeaf.handler.meta) {
+        _.each(currentLeaf.handler.meta, function(val, key) {
+          Ember.$('head').append($("<meta>").attr(key, val));
+        });
+      }
+
+    }
+  }
 });
