4e213d59209f4382c8ff438222144301fa080867
Kyle Davis <validkeys@gmail.com>
2015-06-18 15:38:52 -0400

    Added a meta updater to the people route

--- app/routes/people.js
+++ app/routes/people.js
@@ -9,6 +9,12 @@ export default Ember.Route.extend({
       this.store.pushPayload('person', res);
       return this.store.all('person');
     });
+  },
+
+  actions: {
+    didTransition: function() {
+      $('meta[property="og:name"]').attr('content', 'Acme Co: People');
+    }
   }
 
 });
