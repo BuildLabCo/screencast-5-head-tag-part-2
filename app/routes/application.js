07f3c942608d89697e4876abf4cb42d26890d689
Kyle Davis <validkeys@gmail.com>
2015-06-18 16:22:39 -0400

    Refactored didTransition code into it's own method. Moved inside run loop

--- app/routes/application.js
+++ app/routes/application.js
@@ -12,49 +12,49 @@ export default Ember.Route.extend({
   // so let's refactor a container for our dom elements
   metaElements: [],
 
-  // let's add a cache for our selectors
-  // to make removing them later easier
   metaSelectors: [],
 
-  // a method that will remove the metaSelectors
-  // from the list
   resetMeta: function() {
     let { metaSelectors } = this;
     Ember.$('head').find(metaSelectors.join(',')).remove();
 
-    // reset the metaSelectors array
-    // reset the metaElements array
     this.setProperties({
       metaSelectors: [],
       metaElements:  []
     });
   },
 
+  // moving this out of the didTransition and into its own method
+  setMeta: function() {
+    let handlers          = this.router.get('router.currentHandlerInfos'),
+        currentLeaf       = handlers[handlers.length - 1],
+        { metaElements, metaSelectors }  = this;
+
+    if (currentLeaf.handler.meta) {
+
+      _.each(currentLeaf.handler.meta, function(val, key) {
+        metaSelectors.push(
+          'meta[property="'+key+'"]'
+        );
+        metaElements.push(
+          $("<meta>").attr("property", key).attr("content", val)
+        );
+      });
+
+      Ember.$('head').append(metaElements);
+    }
+  },
+
   actions: {
     didTransition: function() {
 
-      let handlers          = this.router.get('router.currentHandlerInfos'),
-          currentLeaf       = handlers[handlers.length - 1],
-          { metaElements, metaSelectors }  = this;
-
-      if (currentLeaf.handler.meta) {
-
-        _.each(currentLeaf.handler.meta, function(val, key) {
-          metaSelectors.push(
-            'meta[property="'+key+'"]'
-          );
-          metaElements.push(
-            $("<meta>").attr("property", key).attr("content", val)
-          );
-        });
-
-        Ember.$('head').append(metaElements);
-      }
-
+      // Should explain why we should wrap this
+      // in the run loop
+      Ember.run.next(this, this.setMeta);
     },
 
     willTransition: function() {
-      this.resetMeta();
+      Ember.run.next(this, this.resetMeta);
     }
   }
 });
