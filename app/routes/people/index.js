7b70975bf2de23f0a2f0399f56240a109723919a
Kyle Davis <validkeys@gmail.com>
2015-06-18 16:43:20 -0400

    Adding in the current department to the meta but doesnt update when you change departments

--- app/routes/people/index.js
+++ app/routes/people/index.js
@@ -2,17 +2,25 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   
-  // Facebook uses the property key
-  // but other things like twitter use the name key
+  // so now we want to add the department to our meta
+  // but now when we change department filters
+  // the department isn't updating
+  meta: function() {
+    let department = this.controllerFor('people').get('department'),
+        title = "AcmeCo: People";
 
-  meta: {
-    property: {
-      "og:name":  "AcmeCo: People",
-      "og:image": "//www.acmeco.com/logo.png"
-    },
-    name: {
-      "twitter:title": "AcmeCo: People",
-      "twitter:image": "//www.acmeco.com/logo.png"
+    if (department)
+      title += " / " + department;
+
+    return {
+      property: {
+        "og:name":  title,
+        "og:image": "//www.acmeco.com/logo.png"
+      },
+      name: {
+        "twitter:title": title,
+        "twitter:image": "//www.acmeco.com/logo.png"
+      }
     }
   },
 });
